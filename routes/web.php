3e7b678064353427f8518313926f082a578f23a0
Sergio Pajares <spajares@isyc.com>
2021-12-30 19:11:55 +0100

    Cambios en las rutas para que dirijan según los roles a un dashboard o al otro

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,30 @@ use App\Http\Controllers\FilmController;
 use App\Http\Controllers\RolController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
-Route::group(['middleware' => ['auth']], function (){
-   Route::resource('roles',RolController::class);
-   Route::resource('users',UserController::class);
-   Route::resource('films',FilmController::class);
-   Route::resource('home',HomeController::class);
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('roles', RolController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('films', FilmController::class);
+    Route::resource('home', HomeController::class);
 });
 Route::view('/','welcome');
-Route::view('/rented-films','rentedFilms');
+
+Route::group(['middleware' => ['role:Cliente']], function(){
+    Route::get('/available', [App\Http\Controllers\FilmController::class, 'indexFilmAvailable']);
+    Route::view('/available','/films/available');
+    Route::post('available','App\Http\Controllers\FilmController@listFilm');
+    Route::view('/rented-films', 'rentedFilms');
+    Route::post('/home/films-rent','App\Http\Controllers\FilmController@rentFilm');
+    Route::post('/return-films','App\Http\Controllers\FilmController@returnFilm');
+    Route::post('/rented-films/list','App\Http\Controllers\FilmController@listFilmRent');
+});
+
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Route::post('/home','App\Http\Controllers\FilmController@listFilm')->name('listFilm');
-Route::post('/home/films-list','App\Http\Controllers\FilmController@listFilm');
-Route::post('/home/films-list','App\Http\Controllers\FilmController@listFilm');
-Route::post('/home/films-rent','App\Http\Controllers\FilmController@rentFilm');
-Route::post('/return-films','App\Http\Controllers\FilmController@returnFilm');
-Route::post('/rented-films/list','App\Http\Controllers\FilmController@listFilmRent');
+Route::group(['middleware' => ['role:Administrador']], function (){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+
+
+
