7548807b048e0650400c017bfbcdb9394d7aee3f
Sergio Pajares <spajares@isyc.com>
2021-12-30 19:14:07 +0100

    Añadir vista de peliculas disponibles para los clientes

---
+++ resources/views/films/available.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="form rent-form">
+            <table class="table">
+                <tbody>
+                <tr v-for=" film in films ">
+                    <td> @{{ film.name }}</td>
+                    <td>
+                        <button v-on:click="rentFilm(film.id)" type="button" class="btn btn-secondary">
+                            Alquilar
+                        </button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
