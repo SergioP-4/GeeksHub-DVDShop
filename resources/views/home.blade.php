a50409b4e524b5bb4108e366bc44b6dc188293c1
Sergio Pajares <spajares@isyc.com>
2021-12-31 01:02:03 +0100

    Añadimos en el dashboard, los usuarios y las peliculas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="form rent-form">
-            holaaaa
+
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">Dashboard</h3>
         </div>
-    </div>
-</div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="col-md-4 col-xl-4">
+                                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Usuarios</h5>
+                                            @php
+                                                use App\Models\User;
+                                                $cant_users = User::count();
+                                            @endphp
+                                            <p class="card-text">Número de usuarios: {{$cant_users}}
+                                                <br/>
+                                                 <a href="/users" style="color: white;">Ver más</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="card text-white bg-danger mb-3" style="max-width: 18rem">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Peliculas</h5>
+                                            @php
+                                                use App\Models\Film;
+                                                $cant_films = Film::count();
+                                            @endphp
+                                            <p class="card-text">Número de peliculas: {{$cant_films}}
+                                                <br/>
+                                                <a href="/films" style="color: white;">Ver más</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
